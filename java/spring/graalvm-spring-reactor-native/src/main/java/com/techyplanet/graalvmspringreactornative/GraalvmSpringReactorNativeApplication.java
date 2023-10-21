6bf19bf3615f4ff8e439fb98ebab39825e58114f
nitin1988 <sinha.nitin@hotmail.com>
2023-10-21 18:21:15 -0400

    graalvm-spring-reactor-native application generated from spring website

---
+++ java/spring/graalvm-spring-reactor-native/src/main/java/com/techyplanet/graalvmspringreactornative/GraalvmSpringReactorNativeApplication.java
@@ -0,0 +1,13 @@
+package com.techyplanet.graalvmspringreactornative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmSpringReactorNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmSpringReactorNativeApplication.class, args);
+	}
+
+}
---
+++ java/spring/graalvm-spring-reactor-native/src/test/java/com/techyplanet/graalvmspringreactornative/GraalvmSpringReactorNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techyplanet.graalvmspringreactornative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmSpringReactorNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
